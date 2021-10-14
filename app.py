f8d3aa7e89f5f595b7febf95f2e22dda4cef0bd5
Jahimer <Jahimergarcia@hotmail.com>
2021-10-13 21:52:45 -0500

    creacion de la tabla usuario

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask , render_template, url_for
+from datetime import timedelta
+from flask import Flask, render_template, url_for, request, session
 
 app = Flask(__name__)
+app.secret_key = "ASDJSADKJDASKD"
+#app.permanent_session_lifetime = timedelta(minutes= 180)
+
 
 @app.route("/registrar")
 def registrar():
@@ -11,10 +15,17 @@ def recuperarContraseña():
     return render_template("forgot-password.html")
 
 
-@app.route("/ingresar")
+@app.route("/ingresar", methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST" and request.form["email"]: #verifica si es post y si el correo no esta vacio
+        correo= request.form["email"]
+        contraseña = request.form["password"]
+        recordar = request.form["recuerdame"]
+        session["nombre"] = correo
+        return render_template("login.html")
+    else:    
+        return render_template("login.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
